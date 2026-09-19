902c1d28c26765bd5e2d57aef514fe467ba60fa0
AnthonyMh-100 <amh-100@hotmail.com>
2026-09-19 16:09:28 -0500

    feat: add global env config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApplicationsModule } from './applications/applications.module.js';
 
 @Module({
-  imports: [ApplicationsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ApplicationsModule],
   controllers: [],
   providers: [],
 })
